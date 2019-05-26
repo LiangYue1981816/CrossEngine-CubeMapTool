903760a28e026a5eddc9355d5f57e049b2755c2a
LiangYue <LiangYue1981816@Hotmail.com>
2019-05-26 15:03:56 +0800

    - 调整代码

--- CubeMapTool/Texture.cpp
+++ CubeMapTool/Texture.cpp
@@ -7,18 +7,58 @@ texture_type LoadTexture(const char *szFileName)
 	texture_type texture = (texture_type)gli::load(szFileName);
 	if (texture.empty()) return texture_type();
 	if (gli::is_compressed(texture.format())) return texture_type();
-	return gli::convert<texture_type>(texture, gli::FORMAT_RGBA32_SFLOAT_PACK32);
+	return gli::convert<texture_type>(texture, gli::FORMAT_RGB32_SFLOAT_PACK32);
 }
 
-glm::f32vec4 GetTexturePixelColor(const gli::texture2d &texture, int x, int y)
+glm::f32vec3 GetTexturePixelColor(const gli::texture2d &texture, int x, int y)
 {
-	return texture.load<glm::f32vec4>(gli::texture2d::extent_type(x, y), 0);
+	return texture.load<glm::f32vec3>(gli::texture2d::extent_type(x, y), 0);
 }
 
-glm::f32vec4 GetTexturePixelColor(const gli::texture_cube &texture, int x, int y, int face)
+glm::f32vec3 GetTexturePixelColor(const gli::texture_cube &texture, int x, int y, int face)
 {
-	return texture.load<glm::f32vec4>(gli::texture_cube::extent_type(x, y), face, 0);
+	return texture.load<glm::f32vec3>(gli::texture_cube::extent_type(x, y), face, 0);
 }
+
+GLuint CreateTexture2D(const gli::texture2d &texture)
+{
+	gli::gl GL(gli::gl::PROFILE_ES30);
+	gli::gl::format format = GL.translate(texture.format(), texture.swizzles());
+
+	GLuint tex = 0;
+	glGenTextures(1, &tex);
+	glBindTexture(GL_TEXTURE_2D, tex);
+	glTexImage2D(GL_TEXTURE_2D, 0, format.Internal, texture.extent().x, texture.extent().y, 0, format.External, format.Type, texture.data());
+	glBindTexture(GL_TEXTURE_2D, 0);
+	return tex;
+}
+
+GLuint CreateTextureCube(const gli::texture_cube &texture)
+{
+	gli::gl GL(gli::gl::PROFILE_ES30);
+	gli::gl::format format = GL.translate(texture.format(), texture.swizzles());
+
+	GLuint tex = 0;
+	glGenTextures(1, &tex);
+	glBindTexture(GL_TEXTURE_CUBE_MAP, tex);
+	glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X, 0, format.Internal, texture.extent().x, texture.extent().y, 0, format.External, format.Type, texture.data(0, 0, 0));
+	glTexImage2D(GL_TEXTURE_CUBE_MAP_NEGATIVE_X, 0, format.Internal, texture.extent().x, texture.extent().y, 0, format.External, format.Type, texture.data(0, 1, 0));
+	glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_Y, 0, format.Internal, texture.extent().x, texture.extent().y, 0, format.External, format.Type, texture.data(0, 2, 0));
+	glTexImage2D(GL_TEXTURE_CUBE_MAP_NEGATIVE_Y, 0, format.Internal, texture.extent().x, texture.extent().y, 0, format.External, format.Type, texture.data(0, 3, 0));
+	glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_Z, 0, format.Internal, texture.extent().x, texture.extent().y, 0, format.External, format.Type, texture.data(0, 4, 0));
+	glTexImage2D(GL_TEXTURE_CUBE_MAP_NEGATIVE_Z, 0, format.Internal, texture.extent().x, texture.extent().y, 0, format.External, format.Type, texture.data(0, 5, 0));
+	glBindTexture(GL_TEXTURE_CUBE_MAP, 0);
+	return tex;
+}
+
+void DestroyTexture(GLuint tex)
+{
+	glDeleteTextures(1, &tex);
+}
+
+
+// ====
+
 
 void FlipImage(IMAGE *pImage)
 {
@@ -66,9 +106,4 @@ GLuint CreateTextureCube(CUBEMAP *pCubeMap)
 	glBindTexture(GL_TEXTURE_CUBE_MAP, 0);
 
 	return texture;
-}
-
-void DestroyTexture(GLuint texture)
-{
-	glDeleteTextures(1, &texture);
 }

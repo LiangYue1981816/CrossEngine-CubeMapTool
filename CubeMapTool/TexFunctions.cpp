1e8692d25d466416957feaaa940f212a048b9c8e
LiangYue <liangyue1981816@hotmail.com>
2019-05-28 11:51:31 +0800

    - 调整代码

--- CubeMapTool/TexFunctions.cpp
+++ CubeMapTool/TexFunctions.cpp
@@ -94,11 +94,11 @@ glm::f32vec3 GetTexturePixelColor(const gli::texture_cube &texture, const glm::v
 	x = (int)(((sc / ma + 1.0f) * 0.5f) * texture.extent().x);
 	y = (int)(((tc / ma + 1.0f) * 0.5f) * texture.extent().y);
 
-	x = max(x, 0);
-	x = min(x, texture.extent().x - 1);
+	x = std::max(x, 0);
+	x = std::min(x, texture.extent().x - 1);
 
-	y = max(y, 0);
-	y = min(y, texture.extent().y - 1);
+	y = std::max(y, 0);
+	y = std::min(y, texture.extent().y - 1);
 
 	return texture.load<glm::f32vec3>(gli::texture_cube::extent_type(x, y), face, 0);
 }
@@ -159,61 +159,4 @@ gli::texture2d Preview(const gli::texture_cube &cube)
 	}
 
 	return texture;
-}
-
-
-// ====
-
-
-void FlipImage(IMAGE *pImage)
-{
-	for (int y = 0; y < IMAGE_HEIGHT(pImage) / 2; y++) {
-		unsigned char *pPixelTop = pImage->data + pImage->buffWidth * y;
-		unsigned char *pPixelBottom = pImage->data + pImage->buffWidth * (IMAGE_HEIGHT(pImage) - y - 1);
-
-		for (int x = 0; x < pImage->buffWidth; x++) {
-			unsigned char data = pPixelTop[x];
-			pPixelTop[x] = pPixelBottom[x];
-			pPixelBottom[x] = data;
-		}
-	}
-}
-
-GLuint CreateTexture2D(IMAGE *pImage)
-{
-	GLuint texture;
-
-	glGenTextures(1, &texture);
-	glBindTexture(GL_TEXTURE_2D, texture);
-	{
-		FlipImage(pImage);
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, IMAGE_WIDTH(pImage), IMAGE_HEIGHT(pImage), 0, GL_BGR, GL_UNSIGNED_BYTE, pImage->data);
-		FlipImage(pImage);
-	}
-	glBindTexture(GL_TEXTURE_2D, 0);
-
-	return texture;
-}
-
-GLuint CreateTextureCube(CUBEMAP *pCubeMap)
-{
-	GLuint texture;
-
-	glGenTextures(1, &texture);
-	glBindTexture(GL_TEXTURE_CUBE_MAP, texture);
-	{
-		for (int index = 0; index < 6; index++) {
-			FlipImage(&pCubeMap->faces[index]);
-			glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + index, 0, GL_RGB, CUBEMAP_WIDTH(pCubeMap), CUBEMAP_HEIGHT(pCubeMap), 0, GL_BGR, GL_UNSIGNED_BYTE, pCubeMap->faces[index].data);
-			FlipImage(&pCubeMap->faces[index]);
-		}
-	}
-	glBindTexture(GL_TEXTURE_CUBE_MAP, 0);
-
-	return texture;
-}
-
-void DestroyTexture(GLuint texture)
-{
-	glDeleteTextures(1, &texture);
 }
